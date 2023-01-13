b336df2843a4bcab5446829016aaac1c0d60a7ba
Fazil-SA <fazilsa2000@gmail.com>
2023-01-14 01:01:12 +0530

    portfolio temp select template nav

---
+++ client/src/components/user/pt1Demo/SelectTemplate.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { logo } from '../../../assets/user/index'
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import CssBaseline from '@mui/material/CssBaseline';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import Fade from '@mui/material/Fade';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+
+function ScrollTop(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector(
+      '#back-to-top-anchor',
+    );
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        block: 'center',
+      });
+    }
+  };
+
+  return (
+    <Fade in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: 'fixed', bottom: 16, right: 16 }}
+      >
+        {children}
+      </Box>
+    </Fade>
+  );
+}
+
+ScrollTop.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default function BackToTop(props) {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6" component="div">
+            <button className="bg-green-500 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+                {/* <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg> */}
+                <CheckCircleOutlineIcon />
+                <span>Select Template</span>
+            </button>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Toolbar id="back-to-top-anchor" />
+
+      <ScrollTop {...props}>
+        <Fab size="small" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
+    </React.Fragment>
+  );
+}
--- client/src/pages/user/portfolio temps/PortTempDemo1.jsx
+++ client/src/pages/user/portfolio temps/PortTempDemo1.jsx
@@ -6,10 +6,12 @@ import SocialLinks from '../../../components/user/pt1Demo/SocialLinks'
 import Portfolio from '../../../components/user/pt1Demo/Portfolio'
 import Experience from '../../../components/user/pt1Demo/Experience'
 import Contact from '../../../components/user/pt1Demo/Contact'
+import SelectTemplate from '../../../components/user/pt1Demo/SelectTemplate'
 
 const PortTempDemo1 = () => {
   return (
     <div>
+      <SelectTemplate />
       <NavBar />
       <Home />
       <SocialLinks />
